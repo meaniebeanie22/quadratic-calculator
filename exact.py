81405a26b93e43264063a50a8b05031c0891598c
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2022-10-25 09:28:09 +1100

    Disc >= 0 works now

--- exact.py
+++ exact.py
@@ -1,4 +1,5 @@
 import cmath
+from fractions import Fraction
 import math
 
 def gcd(a, b):
@@ -10,28 +11,35 @@ def gcd(a, b):
   return a
 
 def squarefactors(x):
-    return [i for i in range(1,x+1) if x%i==0 and math.sqrt(i)%1==0]
+    return [i for i in range(1,int(x)+1) if x%i==0 and math.sqrt(i)%1==0]
 
 class surdob:
     def __init__(self, surd, mult=1):
         self.surd = surd
-        self.mult = mult
+        self.mult = Fraction('1/1')
 
     def simplify(self):
-        fac = squarefactors(self.surd).sort()
+        fac = squarefactors(self.surd)
         if fac != [1,self.surd]:
-            self.surd = self.surd/squarefactors[1]
-            self.mult = self.mult*math.sqrt(squarefactors[1])
+            self.surd = self.surd/fac[1]
+            self.mult = self.mult*math.sqrt(fac[1])
         return self
     
     def __str__(self):
         if self.mult == 1 and self.surd == 1:
             return '1'
         elif self.mult == 1:
-            return '√'+str(self.surd)
+            return ('√'+str(self.surd))
         elif self.surd == 1:
             return str(self.mult)
+        else:
+            return str(self.mult) + '√' + str(self.surd)
+    
+    def __truediv__(self, other):
+        surd = self.surd
+        mult = self.mult
 
+        return surdob(surd, mult/other)
     
 
 #inputs
@@ -44,12 +52,15 @@ disc = b**2 - 4*a*c
 
 if disc < 0:
     #imaginary numbers
-    pass
-if disc > 0:
+    print('you goofed up - imaginary numbers not supported yet')
+elif disc > 0:
     #real and needs surdification
-    pass
+    discsurd = surdob(b**2 - 4*a*c).simplify()
+    print('Root A =', Fraction(-b/2*a), '+', discsurd/(2*a))
+    print('Root B =', Fraction(-b/2*a), '-', discsurd/(2*a))
+
 else:
     #disc = 0
     g = gcd(-b, 2*a)
-    print('Root is:', (-b/g), '/', (2*a)/g)
+    print('Root is:', Fraction(-b/(2*a)))
 
